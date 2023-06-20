ba7c80ff24ffe13f663366636353cc6c38e63814
ManuMahe <egj.mahe@gmail.com>
2023-06-20 13:45:36 +0200

    Ajout des commentaires

--- Intro/src/Principale.java
+++ Intro/src/Principale.java
@@ -1,6 +1,9 @@
 
-public class Principale { // Ne pas modifier cette ligne
+// Test function
 
+public class Principale { // Ne pas modifier cette ligne
+	
+    //	Method to learn how to use breakpoints for debug
 	public static void main(String[] args) {
 		int i;
 		for (i = 0; i < 5; i++) {
@@ -9,7 +12,9 @@ public class Principale { // Ne pas modifier cette ligne
 
 		System.out.println("C'est la fin !");
 	}
-
+	
+    // Method tested in PrincipaleTest.java
+	// You can use refactor to rename this method and its reuse in others files 
 	public int ajouter(int num1, int num2) {
 		return num1 + num2;
 	}
--- Intro/src/PrincipaleTest.java
+++ Intro/src/PrincipaleTest.java
@@ -1,9 +1,10 @@
+// Import of junit, and of the assertions for testing
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class PrincipaleTest {
-
+	
+    // Method to test method Ajouter() with junit	
 	@Test
 	public void testAjouter() {
 		Principale testIntro = new Principale();
