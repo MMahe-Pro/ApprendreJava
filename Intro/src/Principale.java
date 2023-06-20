555e5beabd6d246e8d63fcfdfb0285148a23159e
ManuMahe <egj.mahe@gmail.com>
2023-06-20 10:34:51 +0200

    Prise en main eclipse 1

---
+++ Intro/src/Principale.java
@@ -0,0 +1,13 @@
+/*
+ * Ce programme s'execute dans la console d'eclipse
+ * Il affiche le texte Hello World à l'écran
+ */
+
+public class Principale { // Ne pas modifier cette ligne
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+
+	}
+
+}
