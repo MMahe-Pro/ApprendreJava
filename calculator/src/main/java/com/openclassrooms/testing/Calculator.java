78ef798ee83c49df0a5cc0a001ab1670c08eaba7
ManuMahe <egj.mahe@gmail.com>
2023-06-27 09:55:25 +0200

    Premiers tests junit sur un projet maven

---
+++ calculator/src/main/java/com/openclassrooms/testing/Calculator.java
@@ -0,0 +1,20 @@
+package com.openclassrooms.testing;
+
+// if you want to launch every tests, you can write "mvn test" in a terminal ...
+// ... or right click on calculator project and run as maven test
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/*
+	 * Method returning the sum of 2 integers
+	 */
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
